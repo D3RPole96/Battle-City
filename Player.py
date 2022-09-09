5406761851a7f69a416299080b00719e37f4d801
D3RPole96 <ilya.lagunov2003@gmail.com>
2022-09-09 19:32:26 +0500

    Player control movement

--- Player.py
+++ Player.py
@@ -1,6 +1,3 @@
-"""
-Objects
-"""
 import os
 import pygame
 
@@ -14,8 +11,27 @@ class Player(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         self.images = []
 
+        self.move_x = 0  # move along X
+        self.move_y = 0  # move along Y
+        self.frame = 0  # count frames
+
         img = pygame.image.load(os.path.join('sprites', 'green-tank.png')).convert()
         self.images.append(img)
         self.size = self.images[0].get_size()
-        self.image = pygame.transform.scale(self.images[0], (int(self.size[0]/4), int(self.size[1]/4)))
+        self.image = pygame.transform.scale(self.images[0], (int(self.size[0] / 4), int(self.size[1] / 4)))
         self.rect = self.image.get_rect()
+
+    """
+    Control player movement
+    """
+
+    def control(self, x, y):
+        self.move_x += x
+        self.move_y += y
+
+    """
+    Update sprite position
+    """
+
+    def update(self):
+        self.rect.x = self.rect.x + self.move_x
